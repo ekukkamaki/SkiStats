c70da3d130da2db1f5a445bf42a04bcfceeb8b69
Esa Kukkamäki <esa.kukkamaki@vitecsoftware.com>
2019-02-19 12:44:39 +0200

    database migraatiot mukaan.

---
+++ SkiApiServer/Migrations/20190219103709_InitialCreate.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SkiApiServer.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rounds",
+                columns: table => new
+                {
+                    RoundId = table.Column<Guid>(nullable: false),
+                    PersonId = table.Column<Guid>(nullable: true),
+                    TotalKilometers = table.Column<int>(nullable: false),
+                    TotalTimeInMinutes = table.Column<int>(nullable: false),
+                    Temperature = table.Column<double>(nullable: false),
+                    SkiStyle = table.Column<int>(nullable: false),
+                    Notes = table.Column<string>(nullable: true),
+                    LocationId = table.Column<Guid>(nullable: true),
+                    ActionTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rounds", x => x.RoundId);
+                    table.ForeignKey(
+                        name: "FK_Rounds_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rounds_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PartialRounds",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    RoundId = table.Column<Guid>(nullable: true),
+                    Length = table.Column<long>(nullable: false),
+                    Duration = table.Column<TimeSpan>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PartialRounds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PartialRounds_Rounds_RoundId",
+                        column: x => x.RoundId,
+                        principalTable: "Rounds",
+                        principalColumn: "RoundId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PartialRounds_RoundId",
+                table: "PartialRounds",
+                column: "RoundId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rounds_LocationId",
+                table: "Rounds",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rounds_PersonId",
+                table: "Rounds",
+                column: "PersonId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PartialRounds");
+
+            migrationBuilder.DropTable(
+                name: "Rounds");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
